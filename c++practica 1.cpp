2a914db677d0458044d2310d977b04b0b216514e
DavidOnt <dajdrontl18@gmail.com>
2020-09-22 12:33:08 -0500

    Ontiveros Lizarraga David Alejandro
    Ontiveros Lizarraga David Alejandro practica 1 - C++

---
+++ c++practica 1.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h>// libreria estandar para realizar funciones en el sistema.
+#include <stdio.h> // libreria para entrada y salida de informacion en la pantalla.
+
+//funcion principal
+int main () {//inicio del programa
+
+//estructura programa.
+printf("Hola a todos \n");
+printf("¿como estan? \n");
+
+system("pause");
+ return 0;
+} // fin del programa.
